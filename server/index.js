67cf520ae2e72ab0b6616554dfc14a7778f932bb
Sambeet Das <sgdas0987@gmail.com>
2025-12-29 12:22:23 +0530

    Add interview API and Express setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("Server starting...");
+const express = require ("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("Interview Platform Server is Running");
+});
+
+app.get("/interviews", (req, res) => {
+    res.json({
+        message: "Interview fetched succesfully",
+        interview: {
+            id: 1,
+            role: "Software Engineer",
+            mode: "Coding Round"
+        }
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
